6c48556ba628179a03b46b4f143b504155e64490
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-31 18:40:02 -0300

    Cria um header para a página (Requisito 2)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <h3 data-testid="email-field">{ email }</h3>
+        <h3 data-testid="total-field">0</h3>
+        <h3 data-testid="header-currency-field">Câmbio: BRL</h3>
+      </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (globalState) => ({
+  email: globalState.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
