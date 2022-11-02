6812451224dd07e219ccd061de9d57cba593f65f
Igor Carvalho <igorr.ackerman@gmail.com>
2022-11-02 19:43:48 -0300

    Adiciona o header da tabela de gastos (Requisito 6)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Routes from './Routes';
+import './App.css';
 
 class App extends React.Component {
   render() {
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div id="tableHeader">
+        <th className="tableTitles">Descrição</th>
+        <th className="tableTitles">Tag</th>
+        <th className="tableTitles">Método de pagamento</th>
+        <th className="tableTitles">Valor</th>
+        <th className="tableTitles">Moeda</th>
+        <th className="tableTitles">Câmbio utilizado</th>
+        <th className="tableTitles">Valor convertido</th>
+        <th className="tableTitles">Moeda de conversão</th>
+        <th className="tableTitles">Editar/Excluir</th>
+      </div>
     );
   }
 }
